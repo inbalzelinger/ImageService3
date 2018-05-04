56676ee81c9b466f976c0b700cfc004def433ccd
inbal zelinger <inbal.zelinger@live.biu.ac.il>
2018-05-04 17:58:59 +0300

    k

---
+++ Gui/ViewModel/SettingsVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Gui.ViewModel
+{
+    class SettingsVM : INotifyPropertyChanged
+    {
+
+        #region Notify Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+
+        private void PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var command = this.SubmitCommand as DelegateCommand<object>;
+            command.RaiseCanExecuteChanged();
+        }
+
+    }
+}
